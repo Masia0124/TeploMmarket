b3b6e191466b84a55ec0a8ab2f83425554746c7a
Masia0124 <mashaplay74@gmail.com>
2026-01-19 11:56:22 +0200

    Сторінка категорії + фільтр

--- js/teplomarket.js
+++ js/teplomarket.js
@@ -30,12 +30,70 @@ $(document).ready(function () {
     })
 
 
-   $(document).on('click', '.dropdawn', function(e){
-     e.preventDefault();
-     let submenu = $(this).children('.dropdawn_list');
-       $('.dropdawn').not(this).removeClass('active').children('.dropdawn_list').slideUp();
-      submenu.slideToggle();
-      $(this).toggleClass('active');
-    });
+    $(".checkbox > i").click(function () {
+        //alert('click');
+        //$('.checkbox_done').toggleClass('checkbox_done_active');
+       $(this).toggleClass('checkbox_done_active');
+    })
+
+
+    $(".filter_mobile").click(function () { 
+  
+        $('.filter_aside').toggleClass('filter_aside_active');
+
+    })
+     
+
+
+
+//    $(document).on('click', '.dropdawn_title', function(e){
+//      e.preventDefault();
+//      let submenu = $(this).children('.dropdawn_list');
+//        $('.dropdawn').not(this).removeClass('active').children('.dropdawn_list').slideUp();
+//       submenu.slideToggle();
+//       $(this).toggleClass('active');
+    //     });
     
+    $(document).on('click', '.filter-title', function (a) { 
+        a.preventDefault();
+
+        let filterDiv = $(this).next('.filter-list');
+        filterDiv.slideToggle();
+    })
+    
+
+
+$(document).on('click', '.dropdawn_title', function (e) {
+      
+    e.preventDefault();
+
+  
+    let submenu = $(this).next('.dropdawn_list');
+    let submenuAll = $('.dropdawn_list');
+    let thisTitle = $(this);
+   
+    submenuAll.not(submenu).slideUp();
+    submenu.slideToggle();
+    $('.dropdawn_title').not(thisTitle).removeClass('active');
+    $(this).toggleClass('active');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
